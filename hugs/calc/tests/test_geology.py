7738d09e78c863d311b5f2edb5ddd5f2fb7be3a1
Bobak <bdkech@gmail.com>
2018-07-10 11:04:35 -0400

    updated tests

--- hugs/calc/tests/test_geology.py
+++ hugs/calc/tests/test_geology.py
@@ -1,7 +1,7 @@
 """Test the `geology` module."""
 
 from hugs.calc import snell_angle
-
+import pytest
 from numpy.testing import assert_almost_equal
 
 
@@ -9,3 +9,10 @@ def test_snell():
     """Test the basic wind component calculation."""
     res = snell_angle(12, 2500, 4000)
     assert_almost_equal(res, 19.43022, 4)
+
+
+def test_snell_zero_velocity_top():
+    """Test that error is raised with 0 velocity top layer"""
+
+    with pytest.raises(ValueError):
+        snell_angle(12, 0, 4800)
